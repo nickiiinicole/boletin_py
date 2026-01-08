47ec5d7bb61da3ee83bbc1246733e5a092d9dd82
nickiiinicole <nicki.nicoledc04@gmail.com>
2026-01-08 10:08:23 +0100

    feat(CODING): Cree la clase Inmobiliaria completa , tocaria hacer los test
    He realizado la clase inmobiliara con sus clases hijas segun la zona de inmueble se creo una funcion para buscar un inmueble en un rango de presupuesto quedaria hacer los test

--- src/boletines/Inmobiliaria.py
+++ src/boletines/Inmobiliaria.py
@@ -11,23 +11,27 @@ class Inmobiliaria():
     def antiguedad(self)->int: 
         return datetime.now().year- self.año
     
-    def precio(self)->float: 
+    def precio_base(self)->float: 
         # acordarse que python deja muitplicar booleanos trnasoformando susvalores en 0 y en 1
-        return (self.metros*1000+self.habitaciones*5000+self.garaje*1500)*1(1-self.antiguedad)
+        valor_garaje = 15000 * self.garaje # True es 1, False es 0
+        return (self.metros*1000+self.habitaciones*5000+valor_garaje)*(1-self.antiguedad)
 
 @dataclass
 class Inmueble_A(Inmobiliaria):
     @property
-    def precioA(self)->float:
-        return self.precio()
+    def precio(self)->float:
+        return self.precio_base()
 
 @dataclass
 class Inmueble_B(Inmobiliaria):
     @property
-    def precioA(self)->float:
-        return self.precio()*1.5
+    def precio(self)->float:
+        return self.precio_base()*1.5
     
 
-def procura_inmueble(lista_pisos: list[Inmobiliaria], pre):
+def buscar_inmueble(lista_apartam: list[Inmobiliaria], presupuesto_max: float):
+    
+    apartam_filtrad = [i for i in lista_apartam if i.precio_base <= presupuesto_max]
 
+    return apartam_filtrad
     
